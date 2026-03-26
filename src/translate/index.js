3b4a9f6fc4b5821686faa23a806d8c5b3b873d46
topshenyi-web <erxu@ctemc.org>
2026-03-26 18:42:23 -0400

    fix: on error, don't translate

--- src/translate/index.js
+++ src/translate/index.js
@@ -8,7 +8,11 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	const TRANSLATOR_API = 'http://17313-team11.s3d.cmu.edu:5000/';
-	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
-	const data = await response.json();
-	return [data.is_english.toString(), data.translated_content];
+	try {
+		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+		const data = await response.json();
+		return [data.is_english.toString(), data.translated_content];
+	} catch (e) {
+		return [true, postData.content]; // default as english
+	}
 };
