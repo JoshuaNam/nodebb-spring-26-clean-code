b7e30509a1451dcd79e05b4713520f562a900174
Yurii Shmal <115579239+yuriiShmal@users.noreply.github.com>
2026-03-26 21:31:21 -0400

    Temporarily Disable Backend Translate

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,15 +4,17 @@
 
 const translatorApi = module.exports;
 
-
-
-translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = 'http://17313-team11.s3d.cmu.edu:5000/';
-	try {
-		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
-		const data = await response.json();
-		return [data.is_english.toString(), data.translated_content];
-	} catch (e) {
-		return [true, postData.content]; // default as english
-	}
+translatorApi.translate = function (postData) {
+	return ['is_english',postData];
 };
+
+// translatorApi.translate = async function (postData) {
+// 	const TRANSLATOR_API = 'http://17313-team11.s3d.cmu.edu:5000/';
+// 	try {
+// 		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+// 		const data = await response.json();
+// 		return [data.is_english.toString(), data.translated_content];
+// 	} catch (e) {
+// 		return [true, postData.content]; // default as english
+// 	}
+// };
